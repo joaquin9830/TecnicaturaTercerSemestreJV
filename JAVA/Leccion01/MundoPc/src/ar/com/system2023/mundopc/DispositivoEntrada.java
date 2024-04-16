a0d6f49d74637d84537122f06bdc7b6e3fac3d4b
Joaquín Viñolo <joaquin9811@gmail.com>
2024-04-16 13:17:45 -0300

    Clase 1  Lección01

---
+++ JAVA/Leccion01/MundoPc/src/ar/com/system2023/mundopc/DispositivoEntrada.java
@@ -0,0 +1,37 @@
+package ar.com.system2023.mundopc;
+
+/**
+  @author marianela
+ */
+public class DispositivoEntrada { /*creamos la clase padre de DispEntr*/
+    private String tipoEntrada;  
+    private String marca;
+    
+    public DispositivoEntrada(String tipoEntrada, String marca){
+        this.tipoEntrada = tipoEntrada;
+        this.marca = marca;
+    }
+
+    public String getTipoEntrada() { /*Agregamos los get/set*/
+        return this.tipoEntrada;
+    }
+
+    public void setTipoEntrada(String tipoEntrada) {
+        this.tipoEntrada = tipoEntrada;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "DispositivoEntrada{" + "tipoEntrada=" + tipoEntrada + ", marca=" + marca + '}';
+    }
+    
+    
+}
