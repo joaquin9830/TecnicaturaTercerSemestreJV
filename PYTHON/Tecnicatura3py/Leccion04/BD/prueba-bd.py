4d023be4bcc8011b1cb5976e34ff074a2003ae59
joaquin9830 <joaquin9811@gmail.com>
2024-06-07 19:42:03 -0300

    Clase 9 Python | Trabajamos la conexión con with

--- PYTHON/Tecnicatura3py/Leccion04/BD/prueba-bd.py
+++ PYTHON/Tecnicatura3py/Leccion04/BD/prueba-bd.py
@@ -7,12 +7,18 @@ conexion = psycopg2.connect(
     port='5432',
     database='test_bd'
 )
-
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia)
-registro = cursor.fetchall()
-print(registro)
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s'
+            id_persona = input('Digite un número para el id_persona: ')
+            cursor.execute(sentencia, (id_persona,))
+            registro = cursor.fetchall()
+            print(registro)
+except Exception as e:
+    print(f'Salió mal por este motivo: {e}')
+finally:
+    conexion.close()
 
 cursor.close()
 conexion.close()
