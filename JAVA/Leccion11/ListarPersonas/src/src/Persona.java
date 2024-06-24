cb5d264ec0e030bac57de906f162921d42af43bf
joaquin9830 <joaquin9811@gmail.com>
2024-06-23 21:23:49 -0300

    Clase 10 Java |Getter and setter de clase persona

--- JAVA/Leccion11/ListarPersonas/src/src/Persona.java
+++ JAVA/Leccion11/ListarPersonas/src/src/Persona.java
@@ -21,4 +21,39 @@ public class Persona {
         this.tel = tel;
         this.email = email;
     }
+    
+    //Getter and setter
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
 }
